81e4acd0a1def7d8e4f1f1816bc7867ad4305e97
Jacob Handwerger <jstudyeth@gmail.com>
2023-01-23 12:01:17 -0500

    added movement to cannon

--- script.js
+++ script.js
@@ -20,25 +20,90 @@ const deck = new Image();
 deck.src = '/images/pirate-ship-deck.png';
 // getting images
 
-// drawing images function
-function drawFrame() {
+let startingX = 180;
+let startingY = 390;
+
+function startGame() {
     ctx.drawImage(oceanBackground, 0, 0, 1500, 769);
     ctx.drawImage(deck, 0, -20, 500, 800);
-    ctx.drawImage(cannonImage, 230, 450, 200, 200);
-    ctx.drawImage(pirateShip, 1200, 100, 100, 200);
+    cannon.draw();
+    cannon.x = startingX;
+    cannon.y = startingY;
 }
-// drawing images function
 
+function updateCanvas() {
+    ctx.clearRect(0,0,1500,769);
+    ctx.drawImage(oceanBackground, 0, 0, 1500, 769);
+    ctx.drawImage(deck, 0, -20, 500, 800);
+    cannon.draw();
+}
 
+// animation 
+function animationLoop() {
+    animationLoopId = setInterval(() => {
+    updateCanvas();
+    }, 500);
+}
+// animation 
+
+const cannon = {
+    x: startingX,
+    y: startingY,
+    width: 300,
+    height: 250,
+  
+    draw: function() {
+      ctx.drawImage(cannonImage, this.x, this.y, this.width, this.height)
+    },
+  
+    moveLeft: function() {
+      this.x = this.x - 5
+    },
+  
+    moveRight: function() {
+      this.x = this.x + 5
+    },
+  
+    moveUp: function() {
+      this.y = this.y - 5
+    },
+  
+    moveDown: function() {
+      this.y = this.y + 5
+    }
+  }
 
 // onload, when you click the button, make the canvas visible, remove the content above, and draw the frame
 window.onload = () => {
     playButton.onclick = () => {
     canvas.style.visibility = 'visible';
     container.remove();
-    drawFrame();
+    startGame();
+    animationLoop();
     }
 }
 // onload, when you click the button, make the canvas visible, remove the content above, and draw the frame
+
+
+
+document.addEventListener('keydown', e => {
+    switch (e.keyCode) {
+      case 38:
+        cannon.moveUp();
+        break;
+      case 40:
+        cannon.moveDown();
+        break;
+      case 37:
+        cannon.moveLeft();
+        break;
+      case 39:
+        cannon.moveRight();
+        break;
+    }
+});
+
+
+
 
 
