707b4cb9108601e9a8d66184954f9c0e82b34388
Jacob Handwerger <jstudyeth@gmail.com>
2023-09-28 19:34:04 -0400

    updates

--- script.js
+++ script.js
@@ -1,63 +1,63 @@
-const playButton = document.getElementById('play-button');
-const menuButton = document.getElementById('menu-button');
-const container = document.getElementById('flex-container');
+const playButton = document.getElementById("play-button");
+const menuButton = document.getElementById("menu-button");
+const container = document.getElementById("home-screen");
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 const oceanBackground = new Image();
-oceanBackground.src = '/images/ocean1.jpg';
+oceanBackground.src = "/images/ocean1.jpg";
 const cannonImage = new Image();
-cannonImage.src = '/images/cannon1.png';
+cannonImage.src = "/images/cannon1.png";
 const cannonBallImage = new Image();
-cannonBallImage.src = '/images/cannon-ball.png';
+cannonBallImage.src = "/images/cannon-ball.png";
 const pirateImage = new Image();
-pirateImage.src = '/images/ship3.png';
+pirateImage.src = "/images/ship.png";
 const deck = new Image();
-deck.src = '/images/pirate-ship-deck.png';
+deck.src = "/images/pirate-ship-deck.png";
 const scoreMap = new Image();
-scoreMap.src = '/images/score-map.png';
+scoreMap.src = "/images/score-map.png";
 const x = new Image();
-x.src = '/images/x.png';
+x.src = "/images/x.png";
 const explosion = new Image();
-explosion.src = '/images/explosion.png';
+explosion.src = "/images/explosion.png";
 
 const fire = new Audio();
-fire.src = 'audio/cannon-fire.mp3';
+fire.src = "audio/cannon-fire.mp3";
 const sailing = new Audio();
-sailing.src = 'audio/sailing-ship.mp3';
+sailing.src = "audio/sailing-ship.mp3";
 sailing.volume = 0.5;
 const winning = new Audio();
-winning.src = 'audio/winning-sound.mp3';
+winning.src = "audio/winning-sound.mp3";
 const laugh = new Audio();
-laugh.src = 'audio/pirate-laugh.mp3';
+laugh.src = "audio/pirate-laugh.mp3";
 
 let cannonballAnimationId;
 let shipCount = 0;
 let score = 0;
 
 function startGame() {
-    ctx.drawImage(oceanBackground, 0, 0, 1500, 769);
-    ctx.drawImage(deck, 0, -20, 500, 800);
-    sailing.play();
-    cannon.draw();
+  ctx.drawImage(oceanBackground, 0, 0, 1500, 769);
+  ctx.drawImage(deck, 0, -20, 500, 800);
+  sailing.play();
+  cannon.draw();
 }
 
 function updateCanvas() {
-    ctx.clearRect(0,0,1500,769);
-    ctx.drawImage(oceanBackground, 0, 0, 1500, 769);
-    ctx.drawImage(deck, 0, -20, 600, 800);     
-    cannonBall1.draw();
-    cannon.draw();
-    checkCollision(cannonBall1, pirateShip1);
-    checkCollision(cannonBall1, pirateShip2);
-    drawScore();
-    newShip(pirateShip1, pirateShip2);
-    scoreChecker();
-  }
+  ctx.clearRect(0, 0, 1500, 769);
+  ctx.drawImage(oceanBackground, 0, 0, 1500, 769);
+  ctx.drawImage(deck, 0, -20, 600, 800);
+  cannonBall1.draw();
+  cannon.draw();
+  checkCollision(cannonBall1, pirateShip1);
+  checkCollision(cannonBall1, pirateShip2);
+  drawScore();
+  newShip(pirateShip1, pirateShip2);
+  scoreChecker();
+}
 
 function updateCannon() {
-  cannonBall1.inFlight = true
+  cannonBall1.inFlight = true;
   cannonballAnimationId = setInterval(() => {
     cannonBall1.move();
     if (cannonBall1.inFlight === true) {
@@ -71,18 +71,27 @@ function updateCannon() {
 
 let animationLoopId;
 
-function animationLoop() {
-    animationLoopId = setInterval(() => {
-      updateCanvas();
-    }, 10);
+let lastTime = 0;
+
+// function animationLoop() {
+//   animationLoopId = setInterval(() => {
+//     updateCanvas();
+//   }, 10);
+// }
+
+function animationLoop(timestamp) {
+  const deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+  updateCanvas();
+  requestAnimationFrame(animationLoop);
 }
 
 class PirateShip {
   constructor() {
-    this.x = 1200
-    this.y = 150
-    this.width = 130
-    this.height = 150
+    this.x = 1200;
+    this.y = 150;
+    this.width = 130;
+    this.height = 150;
   }
 
   draw() {
@@ -90,16 +99,16 @@ class PirateShip {
   }
 
   move() {
-    this.y+=1; 
+    this.y += 1;
   }
   move2() {
-    this.y+=2;
+    this.y += 2;
   }
   move3() {
-    this.y+=3;
+    this.y += 3;
   }
   move4() {
-    this.y+=4
+    this.y += 4;
   }
 }
 
@@ -107,35 +116,41 @@ let pirateShip1 = new PirateShip();
 let pirateShip2 = new PirateShip();
 
 const cannon = {
-    x: 180,
-    y: 530,
-    width: 200,
-    height: 200,
-  
-    draw: function() {
-      ctx.drawImage(cannonImage, this.x, this.y, this.width, this.height);
-    },
-}
+  x: 180,
+  y: 530,
+  width: 200,
+  height: 200,
+
+  draw: function () {
+    ctx.drawImage(cannonImage, this.x, this.y, this.width, this.height);
+  },
+};
 
 class CannonBall {
   constructor(x, y) {
-     this.x = x;
-     this.y = y;
-     this.width = 25;
-     this.height = 25;
-     this.inFlight = false
+    this.x = x;
+    this.y = y;
+    this.width = 25;
+    this.height = 25;
+    this.inFlight = false;
   }
 
   draw() {
-    ctx.drawImage(cannonBallImage, this.x + 160, this.y + 15, this.width, this.height);
+    ctx.drawImage(
+      cannonBallImage,
+      this.x + 160,
+      this.y + 15,
+      this.width,
+      this.height
+    );
   }
 
   move() {
-    this.x+=5.3;
-    if(this.x > canvas.width) {
-      this.x = 180
-      clearInterval(cannonballAnimationId)  
-      this.inFlight = false 
+    this.x += 5.3;
+    if (this.x > canvas.width) {
+      this.x = 180;
+      clearInterval(cannonballAnimationId);
+      this.inFlight = false;
     }
   }
 }
@@ -146,12 +161,18 @@ function checkCollision(ball, ship) {
   ball.top = ball.y + 33;
   ball.bottom = ball.y + ball.height;
   ball.left = ball.x;
-  ball.right = ball.x + ball.width;        
+  ball.right = ball.x + ball.width;
   ship.top = ship.y;
   ship.bottom = ship.y + ship.height;
   ship.left = ship.x;
   ship.right = ship.x + ship.width;
-  if (ball.right > ship.left && ball.top + 20 < ship.bottom && ball.left < ship.right && ball.bottom > ship.top && ship.y < 770) {
+  if (
+    ball.right > ship.left &&
+    ball.top + 20 < ship.bottom &&
+    ball.left < ship.right &&
+    ball.bottom > ship.top &&
+    ship.y < 770
+  ) {
     ctx.drawImage(explosion, 1000, 300, 500, 500);
     cannonBall1.x = 5000;
     score++;
@@ -160,13 +181,13 @@ function checkCollision(ball, ship) {
 }
 
 window.onload = () => {
-    playButton.onclick = () => {
-    canvas.style.visibility = 'visible';
+  playButton.onclick = () => {
+    canvas.style.visibility = "visible";
     container.remove();
     startGame();
-    animationLoop();
-    }
-}
+    requestAnimationFrame(animationLoop);
+  };
+};
 
 function newShip(ship1, ship2) {
   if (ship1.y === 779) {
@@ -175,8 +196,8 @@ function newShip(ship1, ship2) {
   ship1.draw();
   ship1.move();
   if (ship1.y === 800) {
-    ship1.y = 150;                         
-  }                                            
+    ship1.y = 150;
+  }
   if (score >= 2) {
     if (ship2.y === 770) {
       shipCount--;
@@ -202,65 +223,49 @@ function newShip(ship1, ship2) {
 }
 
 function drawScore() {
-  ctx.drawImage(scoreMap, 20, 11, 150, 100); 
-  ctx.fillStyle = "darkGreen"
-  ctx.font = '24px serif'
+  ctx.drawImage(scoreMap, 20, 11, 150, 100);
+  ctx.fillStyle = "darkGreen";
+  ctx.font = "24px serif";
   ctx.fillText(`Score: ${score}`, 55, 45);
   ctx.drawImage(x, 85, 48.5, 20, 20);
-  ctx.fillStyle = "darkRed"
-  ctx.font = '24px serif'
+  ctx.fillStyle = "darkRed";
+  ctx.font = "24px serif";
   ctx.fillText(`Ships: ${shipCount}`, 55, 85);
 }
 
 function scoreChecker() {
-  if (score >= 30) {
-    sailing.volume = 0;
-    fire.volume = 0;
-    winning.play();
-    ctx.fillStyle = 'black'
-    ctx.fillRect(0, 0, 1500, 769)
-    ctx.fillStyle = "green"
-    ctx.font = '150px serif'
-    ctx.fillText(`YOU WIN!`, 390, 400);
-    ctx.drawImage(scoreMap, 20, 11, 150, 100); 
-    ctx.fillStyle = "darkGreen"
-    ctx.font = '24px serif'
-    ctx.fillText(`Score: ${score}`, 55, 45);
-    ctx.drawImage(x, 85, 48.5, 20, 20);
-    cannonBall1.inFlight = true;
-  }
-
-  if (shipCount <= -5 && score < 30) { 
+  if (shipCount <= -5) {
+    console.log("END!!!");
     sailing.volume = 0;
     fire.volume = 0;
     laugh.play();
-    ctx.fillStyle = 'black'
-    ctx.fillRect(0, 0, 1500, 769)
-    ctx.fillStyle = "red"
-    ctx.font = '150px serif'
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 1500, 769);
+    ctx.fillStyle = "red";
+    ctx.font = "150px serif";
     ctx.fillText(`YOU LOSE!`, 370, 400);
     cannonBall1.inFlight = true;
   }
 }
 
-document.addEventListener('keydown', e => {
-    switch (e.keyCode) {
-      case 32:
-        if (cannonBall1.inFlight === false) {
-          updateCannon();
-        }
-        break;
-      case 38:
-        cannon.moveUp();
-        break;
-      case 40:
-        cannon.moveDown();
-        break;
-      case 37:
-        cannon.moveLeft();
-        break;
-      case 39:
-        cannon.moveRight();
-        break;
-    }
+document.addEventListener("keydown", (e) => {
+  switch (e.keyCode) {
+    case 32:
+      if (cannonBall1.inFlight === false) {
+        updateCannon();
+      }
+      break;
+    case 38:
+      cannon.moveUp();
+      break;
+    case 40:
+      cannon.moveDown();
+      break;
+    case 37:
+      cannon.moveLeft();
+      break;
+    case 39:
+      cannon.moveRight();
+      break;
+  }
 });
